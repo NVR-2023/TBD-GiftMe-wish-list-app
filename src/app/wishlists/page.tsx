689cff6810187063eab7c120b14a67645e847a77
NVR <nunoviolanterodrigues@hotmail.com>
2024-01-06 10:24:15 +0000

    detailed layout

--- src/app/wishlists/page.tsx
+++ src/app/wishlists/page.tsx
@@ -8,19 +8,68 @@ export default function Wishlists() {
   const { userDetails, setUserDetails } = useAppContext();
   return (
     <div className="flex flex-col h-screen">
-      <div className="bg-gray-900 text-white p-4">
-        Navbar
-      </div>
+      <div className="bg-gray-900 text-white p-4">Navbar: fixed , fill-width, top</div>
       <div className="flex flex-1">
         <div className="m-1 w-1/5 h-full bg-gray-800 p-4 overflow-y-auto">
-        Sidebar
+          Sidebar: section-width, scrollable vertically
+          <p>Content area</p>
+          <p>Content area</p>
+          <p>Content area</p>
+          <p>Content area</p>
+          <p>Content area</p>
+          <p>Content area</p>
+          <p>Content area</p>
+          <p>Content area</p>
+          <p>Content area</p>
+          <p>Content area</p>
+          <p>Content area</p>
         </div>
         <div className="flex flex-col flex-1">
           <div className="m-1 bg-gray-700 text-white p-4">
-            Section Navbar
+            Section Navbar: fixed, section-width, top
           </div>
-          <div className="m-1 flex-1 bg-green-700 p-4 overflow-y-auto">
-            Content Area
+          <div className="m-1 flex-1 bg-green-700 p-4 h-full overflow-y-auto">
+            Content area: scrollable vertically
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
+            <p>Content Area</p>
           </div>
         </div>
       </div>
