9c7bac6030814555b01ed6928c829c9d1553f423
NVR <nunoviolanterodrigues@hotmail.com>
2024-01-06 09:17:40 +0000

    basic layout structure for wishlists tab

--- src/app/wishlists/page.tsx
+++ src/app/wishlists/page.tsx
@@ -7,7 +7,28 @@ import AddButton from "../_components/Unique/addbutton";
 export default function Wishlists() {
   const { userDetails, setUserDetails } = useAppContext();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="flex flex-col h-screen">
+      <div className="bg-gray-900 text-white p-4">
+        Navbar
+      </div>
+      <div className="flex flex-1">
+        <div className="w-1/5 h-full bg-gray-800 p-4 overflow-y-auto">
+        Sidebar
+        </div>
+        <div className="flex flex-col flex-1">
+          <div className="bg-gray-700 text-white p-4">
+            Section Navbar
+          </div>
+          <div className="flex-1 bg-green-700 p-4 overflow-y-auto">
+            Content Area
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/*     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Wishlists tab successfully connected</div>
       <div>{userDetails.userJWT}</div>
       <Link href="/">Home</Link>
@@ -19,6 +40,4 @@ export default function Wishlists() {
         Set JWT to Wishlists
       </button>
       <AddButton />
-    </main>
-  );
-}
+    </main> */
