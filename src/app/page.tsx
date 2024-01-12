ee1a3fe4fdc72d18a70cef05506d017d3ab8e895
NVR <nunoviolanterodrigues@hotmail.com>
2024-01-12 19:30:07 +0000

    Logo SVG component linked. File cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import GiftMeLogo from "./_components/Common/GiftMeLogo";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>GiftMe next,js App in development</div>
+      <div>
+        <GiftMeLogo height="200px" width="200px" />
+      </div>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <Link href="/wishlists">Wishlists</Link>
         <Link href="/groups">Groups</Link>
-        <Link href="/notifications">Notifications</Link> 
+        <Link href="/notifications">Notifications</Link>
       </div>
     </main>
-  )
+  );
 }
