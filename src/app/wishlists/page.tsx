426d30aacf5249b16dd06221ad7d1205842e0e1a
NVR <nunoviolanterodrigues@hotmail.com>
2024-01-12 16:45:18 +0000

    basic layout of wishlists

--- src/app/wishlists/page.tsx
+++ src/app/wishlists/page.tsx
@@ -2,74 +2,33 @@
 
 import Link from "next/link";
 import { useAppContext } from "@/context";
-import AddButton from "../_components/Unique/addbutton";
+import NavBar from "../_components/Layout/navbar/navbar";
 
 export default function Wishlists() {
   const { userDetails, setUserDetails } = useAppContext();
+
+  const SideBar = () => {
+    return (
+      <div>
+        SideBAr
+      </div>
+    )
+
+  };
+
+  const FilterBar = () => {
+    return <div className="">Filterbar</div>;
+  };
+
   return (
-    <div className="flex flex-col h-screen">
-      <div className="bg-gray-900 text-white p-4">Navbar: fixed , fill-width, top</div>
-      <div className="flex flex-1">
-        <div className="m-1 w-1/5 h-full bg-gray-800 p-4 overflow-y-auto">
-          Sidebar: section-width, scrollable vertically
-          <p>Content area</p>
-          <p>Content area</p>
-          <p>Content area</p>
-          <p>Content area</p>
-          <p>Content area</p>
-          <p>Content area</p>
-          <p>Content area</p>
-          <p>Content area</p>
-          <p>Content area</p>
-          <p>Content area</p>
-          <p>Content area</p>
-        </div>
-        <div className="flex flex-col flex-1">
-          <div className="m-1 bg-gray-700 text-white p-4">
-            Section Navbar: fixed, section-width, top
-          </div>
-          <div className="m-1 flex-1 bg-green-700 p-4 h-full overflow-y-auto">
-            Content area: scrollable vertically
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
-            <p>Content Area</p>
+    <div>
+      <NavBar />
+      <div className="relative top-16 lg:flex">
+        <SideBar />
+        <div className="w-full h-full p-1 m-1">
+          <FilterBar />
+          <div className="items-center justify-center max-h-[calc(100vh-120px)] overflow-y-auto p-8 mr-5 border-4 lg:p-10">
+            123
           </div>
         </div>
       </div>
