8e03b88e7f13de4e4ea67f58e82a593ef8c60c1b
NVR <nunoviolanterodrigues@hotmail.com>
2024-01-12 18:44:23 +0000

    Cleaned up home page. Detailed Global Context for all tabs

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,20 +1,102 @@
 "use client";
 import { createContext, useState, useContext } from "react";
 
+// Wishlists
+
+type gift = {
+  giftId?: number;
+  name?: string;
+  category?: string;
+  giftImage?: string;
+  provider?: string;
+  providerURL?: string;
+  isSentToMe?: boolean;
+  status?: string;
+  price?: number;
+  isPriceTracked?: boolean;
+  upperPriceLimit?: number;
+  lowerPriceLimit?: number;
+  deliveryDate?: Date;
+  maxDeliveryDate?: Date;
+  registerDate?: Date;
+  giftedByUser?: string;
+}
+
+type myWishlist = {
+  name?: string;
+  giftArray?: gift[];
+}
+
+type externalWishlist = {
+  name?: string;
+  giftArray?: gift[];
+  receiver?: string;
+};
+
+type displayWishlistBy = {
+  status?: string;
+  price?: string;
+  category?: string;
+  searchBy?: string;
+}
+
+// Groups
+
+type gifter = {
+  name?: string;
+  avatarImage?: string;
+}
+
+type group = {
+  name?: string,
+  gifterArray?: gifter[];
+}
+
+//Notifications
+
+type notification = {
+  notificationContent?: string;
+  isRead?: boolean;
+  sent?: Date;
+}
+
 type AppContextProps = {
+  //General user details
   userId?: number;
   userName?: string;
   userJWT?: string;
-  currentPage?: string,
+  currentPage?: string;
   darkMode?: boolean;
+
+  //Wishlists tab
+  sortWishlistsBy?: string;
+  myWIshListArray?: myWishlist[];
+  externalWishlistArray?: externalWishlist[];
+  currentWishlistType?: "myWishlists" | "externalWishlists";
+  currentWishlistIndex?: number;
+
+  displayWishlistBy?: displayWishlistBy;
+
+  //Groups tab
+  sortGroupsBy?: string;
+  groupsArray?: group[];
+  sortGiftersBy?: string
+  searchGroupBy?: string;
+
+  //Notifications tab
+  sortNotificationsBy?: string;
+  searchNotificationsBy?: string;
+  notificationsArray?: notification[];
+
 };
+
 
 const defaultAppContext: AppContextProps = {
   userId: 333,
   userName: "John",
-  userJWT: "- Default state -",
+  userJWT: "- Default Token -",
   currentPage: "wishlists",
-  darkMode: true,
+  darkMode: false,
 };
 
 // revision: argument needs to be typed
