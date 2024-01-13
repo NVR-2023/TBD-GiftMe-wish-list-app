52a769b9829bceff395e57bdcfb502e552e78add
NVR <nunoviolanterodrigues@hotmail.com>
2024-01-13 15:00:47 +0000

    Daisy UI connected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,24 @@ export default function Home() {
       <div>
         <GiftMeLogo height="200px" width="200px" />
       </div>
+      <div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            Daisy UI connected
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              Card component without image
+              <div className="badge badge-secondary">NEW</div>
+            </h2>
+            <p>Test</p>
+            <div className="card-actions justify-end">
+              <div className="badge badge-outline">UI</div>
+              <div className="badge badge-outline">TailwindCSS</div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="z-10 max-w-5xl w-full flex items-center justify-evenly font-mono text-sm lg:w-auto">
         <Link className="text-green-400 pe-9" href="/wishlists">
           Wishlists
