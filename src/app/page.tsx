e0ef067062d106bf3dcb20db0d9302ead0bb5d4d
NVR <nunoviolanterodrigues@hotmail.com>
2024-01-13 13:24:02 +0000

    Reverted back to rpevious commit. Testing TailwinfCSS link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,20 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>GiftMe next,js App in development</div>
+      <div>GiftMe next.js App in development</div>
       <div>
         <GiftMeLogo height="200px" width="200px" />
       </div>
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <Link href="/wishlists">Wishlists</Link>
-        <Link href="/groups">Groups</Link>
-        <Link href="/notifications">Notifications</Link>
+      <div className="z-10 max-w-5xl w-full flex items-center justify-evenly font-mono text-sm lg:w-auto">
+        <Link className="text-green-400 pe-9" href="/wishlists">
+          Wishlists
+        </Link>
+        <Link className="text-blue-400 pe-9" href="/groups">
+          Groups
+        </Link>
+        <Link className="text-red-400" href="/notifications">
+          Notifications
+        </Link>
       </div>
     </main>
   );
